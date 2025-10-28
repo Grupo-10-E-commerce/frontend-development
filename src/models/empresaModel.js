ae50d1929e7b20529afadff54ffb5006a7af45ac
EduardoGomesdeCamargo <eduardo.camargo@sptech.school>
2025-10-28 05:09:12 -0300

    ajuste - cadastro de empresa e usuários

--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -1,19 +1,19 @@
 var database = require("../database/config");
 
 function buscarPorId(id) {
-  var instrucaoSql = `SELECT * FROM empresa WHERE id = '${id}'`;
+  var instrucaoSql = `SELECT * FROM empresa WHERE id = '${id}'`; 
 
   return database.executar(instrucaoSql);
 }
 
 function listar() {
-  var instrucaoSql = `SELECT id, razao_social, cnpj, codigo_ativacao FROM empresa`;
+  var instrucaoSql = `SELECT id, razao_social, cnpj FROM empresa`;
 
   return database.executar(instrucaoSql);
 }
 
 function buscarPorCnpj(cnpj) {
-  var instrucaoSql = `SELECT * FROM empresa WHERE cnpj = '${cnpj}'`;
+  var instrucaoSql = `SELECT id_empresa FROM empresa WHERE cnpj = '${cnpj}'`;
 
   return database.executar(instrucaoSql);
 }
--- src/models/usuariosModel.js
+++ src/models/usuariosModel.js
@@ -11,7 +11,7 @@ return database.executar(instrucao);
 
 function cadastrar(nome, email, senha){
     var instrucao = `
-    INSERT INTO usuario (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
+    INSERT INTO usuario (nome, email, senha, id_empresa, id_cargo) VALUES ('${nome}', '${email}', '${senha}', 1, 1);
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);

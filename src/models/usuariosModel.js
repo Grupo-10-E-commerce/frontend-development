61e17d0b2c759d6b6091a9586f2a2ee1a523d4bd
EduardoGomesdeCamargo <eduardo.camargo@sptech.school>
2025-11-20 01:24:29 -0300

    ajustes - cadastrar usuário agora insere o id da empresa criada no banco na tela de cadastro; adiciona limite de caractere - 14 - na input cnpj

--- src/models/usuariosModel.js
+++ src/models/usuariosModel.js
@@ -9,9 +9,9 @@ return database.executar(instrucao);
 
 }
 
-function cadastrar(nome, email, senha){
+function cadastrar(nome, email, senha, id_empresa){
     var instrucao = `
-    INSERT INTO usuario (nome, email, senha, id_empresa, id_cargo) VALUES ('${nome}', '${email}', '${senha}', 1, 1);
+    INSERT INTO usuario (nome, email, senha, id_empresa, id_cargo) VALUES ('${nome}', '${email}', '${senha}', ${id_empresa}, 1);
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
